bae2bc60ae0fb6c0ce26dad41614a62312d2ed48
Eloise Gagne <eloise_gagne@hotmail.com>
2022-11-03 21:16:29 -0400

    swipper_pas_fonctionnel

--- acceuil/pages/page_produit/produits.js
+++ acceuil/pages/page_produit/produits.js
@@ -33,3 +33,18 @@ jQuery('.quantity').each(function() {
 });
 
 // Swiper //
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
