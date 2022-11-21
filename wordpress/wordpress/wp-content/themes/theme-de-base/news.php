71ab4aac0cd9cad7f8eff9a58a3e78e10b7cb7b7
Eloise Gagne <eloise_gagne@hotmail.com>
2022-11-21 11:44:53 -0500

    news_ajustements

--- wordpress/wordpress/wp-content/themes/theme-de-base/news.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/news.php
@@ -6,11 +6,24 @@
 get_header();
 
 ?>
-<main>
-    <h1>Nouvelles</h1>
-<?php xo_slider( 104 ); ?>
 
-</main>
+    <h1 class="nouvelles_titre">Nouvelles</h1>
+    <div class="big_slider">
+<?php xo_slider( 104 ); ?>
+</div>
+<style>
+    .big_slider {
+        margin-bottom:50px
+    }
+
+    .slide-content-title, .slide-content-subtitle {
+    color:black;
+    }
+    
+    .nouvelles_titre {
+        margin-top:30px;
+    }
+</style>
 <?php xo_slider( 107 ); ?>
 <?php 
 
