ded9b81ab8060368ea7628a9ab02f95283f77265
Eloise Gagne <eloise_gagne@hotmail.com>
2022-11-26 13:15:51 -0500

    typo

--- wordpress/wordpress/wp-content/themes/theme-de-base/header.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/header.php
@@ -22,7 +22,9 @@
 	 Supprimer cette fonction briserait vos extensions et diverses fonctionnalités WordPress. 
 	 Vous pouvez la déplacer si désiré, mais garder là. */
 ?>
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Salsa&display=swap');
+</style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
