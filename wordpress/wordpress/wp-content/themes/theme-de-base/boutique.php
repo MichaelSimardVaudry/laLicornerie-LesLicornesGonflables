ae4de70b3d7ae56b70ebf0547c6feb036ade7208
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-09 13:42:56 -0500

    boutique

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -6,20 +6,24 @@ get_header();
 
 $products = array('post_type' => array('jouet', 'bijoux', 'accessoir', 'comestible'));
 
-$query = new WP_Query($products);
-while($query->have_posts()) : $query->the_post();
 ?>
 
 <main>
     <article class='lesProduits'>
-      <div class='produit'>
-        <img class='imgProduit'
-          src='<?php the_field('image_produit') ; ?>'>
-          <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
+
+        <?php
+$query = new WP_Query($products);
+while($query->have_posts()) : $query->the_post();
+?>
+
+
+        <div class='produit'>
+            <img class='imgProduit' src='<?php the_field('image_produit') ; ?>'>
+            <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
             <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
-       </div>
+        </div>
         <?php  endwhile; wp_reset_postdata(); ?>
-     </article>
+    </article>
 </main>
 
 <?php get_footer(); ?>
