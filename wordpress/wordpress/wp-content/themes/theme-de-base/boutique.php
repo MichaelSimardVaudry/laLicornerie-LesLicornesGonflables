1cb3ec21f3591f0af95f4e55c02d3ba37a34e90d
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-09 09:43:15 -0500

    ddd

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -1,39 +1,17 @@
 <?php 
 /**
  * 	Template Name: Boutique
- *  Template Post Type: comestibles, jouets, bijoux, accessoires
+ *  Template Post Type: comestibles, jouet, bijoux, accessoires
  */
 
-$jouets = new WP_Query('post_type=jouets');
+$jouet = new WP_Query('post_type=jouet');
 
 get_header();
 ?>
 
 <main>
-    <div class='searchingBoutique'>
-    <label>
-      Age:
-      <select name='Age:'>
-        <option value="4">0 à 4 ans</option>
-        <option value="8">5 à 8 ans</option>
-        <option value="9">9 à 18 ans</option>
-        <option value="adulte">Adultes</option>
-      </select>
-    </label>
-    
-    <label>
-      Tier par:
-      <select name='Trier par:'>
-        <option value="default">Par défault</option>
-        <option value="nouveautes">Nouveautés</option>
-        <option value="croissant">Prix croissant</option>
-        <option value="decroissant">Prix décroissant</option>
-      </select>
-    </label>
-  </div>
-    
     <article class='lesProduits'>
-        <?php if ($jouets->have_posts()) : while($jouets->have_posts()) : $jouets->the_post(); ?>
+        <?php if ($jouet->have_posts()) : while($jouet->have_posts()) : $jouet->the_post(); ?>
       <div class='produit'>
         <img class='imgProduit'
           src='<?php the_field('image_produit') ; ?>'>
