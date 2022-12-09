cb17870b62044b1a1775192b63196099584a4acf
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-09 13:52:31 -0500

    bouton

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -21,6 +21,7 @@ while($query->have_posts()) : $query->the_post();
             <img class='imgProduit' src='<?php the_field('image_produit') ; ?>'>
             <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
             <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
+            <button type="button" class='boutiqueBouton'>Ajouter au Panier</button>
         </div>
         <?php  endwhile; wp_reset_postdata(); ?>
     </article>
