af1411ff4e4c5a5851ec459ddf47c32118af77e3
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-09 10:14:55 -0500

    yeah y yeah

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -11,14 +11,13 @@ while($products->have_posts()) : $products->the_post();
 
 <main>
     <article class='lesProduits'>
-      <div class='produit'>
-        <img class='imgProduit'
-          src='<?php the_field('image_produit') ; ?>'>
-          <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
+        <div class='produit'>
+            <img class='imgProduit' src='<?php the_field('image_produit') ; ?>'>
+            <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
             <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
-       </div>
-         endwhile; wp_reset_postdata(); 
-     </article>
+        </div>
+        <?php endwhile; wp_reset_postdata();?>
+    </article>
 </main>
 
 <?php 
