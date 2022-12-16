5ef7b541b8a4d8197960f0fc401c89ef21d6691d
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-16 00:06:07 -0500

    classname

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -10,10 +10,10 @@ $products = array('post_type' => array('jouet', 'bijoux', 'accessoir', 'comestib
 
 <main>
     <div class="categories" style="padding: 20px;">
-        <a class='boutiqueBouton' href="jouet">Jouets</a>
-        <a class='boutiqueBouton' href="bijoux">Bijoux</a>
-        <a class='boutiqueBouton' href="accesoir">Accessoires</a>
-        <a class='boutiqueBouton' href="comestible">Comestibles</a>
+        <a class='boutiqueCategory' href="jouet">Jouets</a>
+        <a class='boutiqueCategory' href="bijoux">Bijoux</a>
+        <a class='boutiqueCategory' href="accesoir">Accessoires</a>
+        <a class='boutiqueCategory' href="comestible">Comestibles</a>
     </div>
 
     <article class='lesProduits'>
