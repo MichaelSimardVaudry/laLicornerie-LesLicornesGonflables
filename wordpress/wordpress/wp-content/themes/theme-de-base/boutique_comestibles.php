70989edc41ffb7c6027640b1c27cefe24dbd0c06
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-16 10:29:50 -0500

    as

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique_comestibles.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique_comestibles.php
@@ -21,9 +21,7 @@ $products = array('post_type' => array('comestible'));
                 <?php $query = new WP_Query($products);
         while($query->have_posts()) : $query->the_post(); ?>
                 <div class='produit'>
-                    <a
-                        href="http://localhost/laLicornerie-LesLicornesGonflables/wordpress/wordpress/article/<?php the_field('titre')?>"><img
-                            class='imgProduit' src='<?php the_field('image_produit') ; ?>'></a>
+                    <a href="rubarbe"><img class='imgProduit' src='<?php the_field('image_produit') ; ?>'></a>
                     <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
                     <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
                     <button type="button" class='boutiqueBouton'>Ajouter au Panier</button>
--- wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,25 +1,76 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js" integrity="sha512-gmwBmiTVER57N3jYS3LinA9eb8aHrJua5iQD7yqYCKa5x6Jjc7VDVaEA0je0Lu0bP9j7tEjV3+1qUm6loO99Kw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<section>
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e1">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e2">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e3">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e4">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e5">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e6">
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e7"> 
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e8"> 
-  <h1>Erreur 404</h1>
-  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/dessinLicorne.png" class="dessinLicorne">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"
+    integrity="sha512-gmwBmiTVER57N3jYS3LinA9eb8aHrJua5iQD7yqYCKa5x6Jjc7VDVaEA0je0Lu0bP9j7tEjV3+1qUm6loO99Kw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer">
+;
+</script>
+<section class="page_404">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e1">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e2">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e3">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e4">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e5">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e6">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e7">
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png"
+        class="etoile e8">
+    <h1>Erreur 404</h1>
+    <h2>Oops pas de page</h2>
+    <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/dessinLicorne.png"
+        class="dessinLicorne">
 </section>
 
 <script>
 const anim1 = gsap.timeline()
-.to('.e1', { y: '750px',duration: 3,})
-.to('.e2', { y: '750px',duration: 3,}, '-=2.9')
-.to('.e3', { y: '750px',duration: 3,}, '-=3.2')
-.to('.e4', { y: '750px',duration: 3,}, '-=3.5')
-.to('.e5', { y: '750px',duration: 3,}, '-=2.8')
-.to('.e6', { y: '750px',duration: 3,}, '-=3.7')
-.to('.e7', { y: '750px',duration: 3,}, '-=3.2')
-.to('.e8', { y: '750px',duration: 3,}, '-=3.7');
+    .to('.e1', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    })
+    .to('.e2', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=2.9')
+    .to('.e3', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=3.2')
+    .to('.e4', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=3.5')
+    .to('.e5', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=2.8')
+    .to('.e6', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=3.7')
+    .to('.e7', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=3.2')
+    .to('.e8', {
+        y: '750px',
+        duration: 3,
+        repeat: -1
+    }, '-=3.7')
+    .to('.dessinLicorne', {
+        x: '-100vw',
+        duration: 3,
+        repeat: -1
+    }, '-=3.7');
 </script>
