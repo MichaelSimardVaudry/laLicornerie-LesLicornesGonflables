77a91cd99bb01aac8312a7379a490e682057c051
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-15 20:23:56 -0500

    erreur404 GSAP

--- wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,4 +1,25 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js" integrity="sha512-gmwBmiTVER57N3jYS3LinA9eb8aHrJua5iQD7yqYCKa5x6Jjc7VDVaEA0je0Lu0bP9j7tEjV3+1qUm6loO99Kw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e1">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e2">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e3">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e4">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e5">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e6">
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e7"> 
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/star.png" class="etoile e8"> 
+  <h1>Erreur 404</h1>
+  <img src="https://raw.githubusercontent.com/MichaelSimardVaudry/laLicornerie-LesLicornesGonflables/main/medias/dessinLicorne.png" class="dessinLicorne">
 </section>
+
+<script>
+const anim1 = gsap.timeline()
+.to('.e1', { y: '750px',duration: 3,})
+.to('.e2', { y: '750px',duration: 3,}, '-=2.9')
+.to('.e3', { y: '750px',duration: 3,}, '-=3.2')
+.to('.e4', { y: '750px',duration: 3,}, '-=3.5')
+.to('.e5', { y: '750px',duration: 3,}, '-=2.8')
+.to('.e6', { y: '750px',duration: 3,}, '-=3.7')
+.to('.e7', { y: '750px',duration: 3,}, '-=3.2')
+.to('.e8', { y: '750px',duration: 3,}, '-=3.7');
+</script>
