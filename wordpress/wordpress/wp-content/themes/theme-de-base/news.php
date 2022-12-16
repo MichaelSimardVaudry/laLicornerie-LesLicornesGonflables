51daa6321e3c0cbec8d548b4819a6debf1568aab
Eloise Gagne <eloise_gagne@hotmail.com>
2022-12-15 20:39:07 -0500

    Update news.php

--- wordpress/wordpress/wp-content/themes/theme-de-base/news.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/news.php
@@ -9,15 +9,14 @@ $news = array('post_type' => array('Nouvelle'));
 ?>
 
     <h1 class="nouvelles_titre">Nouvelles</h1>
-    <div class="container">
+    <div>
     <?php $query = new WP_Query($news);
         while($query->have_posts()) : $query->the_post(); ?>
-
-            <img src="<?php the_field(''); ?>">
+            <img src="<?php the_field('image'); ?>">
             <div class=''><?php the_field('titre') ; ?></div>
             <div class=''><?php the_field('description') ; ?></div>
 </div>
-</div>
+
 
 
 <?php 
