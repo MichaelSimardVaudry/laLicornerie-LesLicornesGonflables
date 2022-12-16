e96d63e740b522abdc16729c289d1afc5e03dded
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-15 22:42:16 -0500

    backtonormal

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -5,27 +5,10 @@
 get_header();
 
 $products = array('post_type' => array('jouet', 'bijoux', 'accessoir', 'comestible'));
-$jouets = array('post_type' => array('jouet'));
-$bijoux = array('post_type' => array('bijoux'));
-$accessoires = array('post_type' => array('accessoir'));
-$comestibles = array('post_type' => array('comestible'));
 
 ?>
 
 <main>
-  <div class='searchingBoutique'>
-    <label>
-      Catégories:
-      <select name='category' id='boutiqueCategory'>
-        <option value="default">Par défault</option>
-        <option value="toy">Jouets</option>
-        <option value="jewlery">Bijoux</option>
-        <option value="other">Accessoires</option>
-        <option value="food">Comestible</option>
-      </select>
-    </label>
-  </div>
-    
     <article class='lesProduits'>
     <?php $query = new WP_Query($products);
         while($query->have_posts()) : $query->the_post(); ?>
