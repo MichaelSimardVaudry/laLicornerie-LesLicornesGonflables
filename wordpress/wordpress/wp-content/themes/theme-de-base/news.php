a1e60dc828b0bfec2b8ff3e9ac152894fde4d71b
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-16 08:14:05 -0500

    as

--- wordpress/wordpress/wp-content/themes/theme-de-base/news.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/news.php
@@ -17,16 +17,17 @@ $last = array(
     <h5> Dernière nouvelle:</h5>
     <?php $query = new WP_Query($last);
         while($query->have_posts()) : $query->the_post(); ?>
-    <div class="row last_news">
-        <div class="col-12 col-sm-6 col-lg-3">
-            <a href="<?php the_field('link'); ?>"><img src="<?php the_field('image'); ?>" class="img-fluid"></a>
-        </div>
-        <div class="col-7">
-            <h3 class="card-title"><?php the_field('nom') ; ?></h3>
-            <div class="card-body"><?php the_field('description') ; ?>
-                </br><?php the_field('date') ; ?>
+    <div>
+        <div class="row last_news">
+            <div class="col-12 col-sm-6 col-lg-3">
+                <a href="<?php the_field('link'); ?>"><img src="<?php the_field('image'); ?>" class="img-fluid"></a>
             </div>
-
+            <div class="col-7">
+                <h3 class="card-title"><?php the_field('nom') ; ?></h3>
+                <div class="card-body"><?php the_field('description') ; ?>
+                    </br><?php the_field('date') ; ?>
+                </div>
+            </div>
         </div>
     </div>
     <?php  endwhile; wp_reset_postdata(); ?>
