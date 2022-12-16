4d193bd6848657a41dffd99dd0b1afcb9b475895
Eloise Gagne <eloise_gagne@hotmail.com>
2022-12-15 21:36:00 -0500

    index-article, css card

--- wordpress/wordpress/wp-content/themes/theme-de-base/article.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/article.php
@@ -36,23 +36,7 @@ get_header();
     </div>
 
 </main>
-<!-- Swiper -->
-<div class="swiper mySwiper">
-    <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
-    </div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-pagination"></div>
-</div>
+
 
 <div class="card text">
     <div class="card-header">
