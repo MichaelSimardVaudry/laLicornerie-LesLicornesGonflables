7b965f7b80623cb112272314c97aa209631f3c02
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-15 20:44:42 -0500

    fermer laboucle

--- wordpress/wordpress/wp-content/themes/theme-de-base/news.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/news.php
@@ -8,14 +8,18 @@ $news = array('post_type' => array('Nouvelle'));
 
 ?>
 
-    <h1 class="nouvelles_titre">Nouvelles</h1>
-    <div class="container">
+<h1 class="nouvelles_titre">Nouvelles</h1>
+<div class="container">
     <?php $query = new WP_Query($news);
         while($query->have_posts()) : $query->the_post(); ?>
 
-            <img src="<?php the_field(''); ?>">
-            <div class=''><?php the_field('titre') ; ?></div>
-            <div class=''><?php the_field('description') ; ?></div>
+    <img src="<?php the_field('image') ; ?>">
+    <div class=''><?php the_field('titre')?></div>
+    <div class=''><?php the_field('description')?></div>
+    <?php
+                    endwhile; 
+                    wp_reset_postdata(); 
+                ?>
 </div>
 </div>
 
