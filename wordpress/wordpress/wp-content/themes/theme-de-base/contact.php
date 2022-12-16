7d4b6bdd23d9c5f4cb9ccb36284f94065507754e
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-15 22:08:15 -0500

    oops la faute

--- wordpress/wordpress/wp-content/themes/theme-de-base/contact.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/contact.php
@@ -385,7 +385,7 @@ get_header();
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <H2>Équipe derrier le site:</H2>
+                        <H2>Équipe derrière le site:</H2>
                     </div>
                 </div>
                 <div class="row">
