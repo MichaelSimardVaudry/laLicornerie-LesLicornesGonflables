ed1e26adc722410019278c3e4526e7aa4f5e580f
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-15 23:03:06 -0500

    add comestibles

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -10,16 +10,32 @@ $products = array('post_type' => array('jouet', 'bijoux', 'accessoir', 'comestib
 
 <main>
     <article class='lesProduits'>
-    <?php $query = new WP_Query($products);
+        <?php $query = new WP_Query($products);
         while($query->have_posts()) : $query->the_post(); ?>
         <div class='produit'>
-            <a href="http://localhost/laLicornerie-LesLicornesGonflables/wordpress/wordpress/article/"><img class='imgProduit' src='<?php the_field('image_produit') ; ?>'></a>
+            <a
+                href="http://localhost/laLicornerie-LesLicornesGonflables/wordpress/wordpress/article/<?php the_field('titre')?>"><img
+                    class='imgProduit' src='<?php the_field('image_produit') ; ?>'></a>
             <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
             <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
             <button type="button" class='boutiqueBouton'>Ajouter au Panier</button>
         </div>
         <?php  endwhile; wp_reset_postdata(); ?>
-        
+        <?php $arguments = array(
+                    'post_type' => 'comestible'
+                );
+                $comestible = new WP_Query($arguments);
+                while ($comestible->have_posts()) : $comestible->the_post(); ?>
+        <div class='produit'>
+            <a
+                href="http://localhost/laLicornerie-LesLicornesGonflables/wordpress/wordpress/article/<?php the_field('titre')?>"><img
+                    class='imgProduit' src='<?php the_field('image_produit') ; ?>'></a>
+            <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
+            <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
+            <button type="button" class='boutiqueBouton'>Ajouter au Panier</button>
+        </div>
+        <?php  endwhile; wp_reset_postdata(); ?>
+
     </article>
 </main>
 
