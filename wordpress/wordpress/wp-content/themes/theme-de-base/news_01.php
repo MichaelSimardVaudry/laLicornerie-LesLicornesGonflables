32f6dcd14b8cfab3a22f2c31216e10d75e11f56b
MichaelSimardVaudry <78884924+MichaelSimardVaudry@users.noreply.github.com>
2022-12-15 22:34:50 -0500

    asas

--- wordpress/wordpress/wp-content/themes/theme-de-base/news_01.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/news_01.php
@@ -8,13 +8,16 @@ $news = array('post_type' => array('Nouvelle'));
 
 ?>
 <div>
-<?php $query = new WP_Query($news);
+    <?php $query = new WP_Query($news);
         while($query->have_posts()) : $query->the_post(); ?>
     <h1><?php the_field('nom') ; ?></h1>
     <div><?php the_field('description') ; ?></div>
-   
-</div>
 
+</div>
+<?php
+                    endwhile; 
+                    wp_reset_postdata(); 
+                ?>
 <?php 
 
 
