d739c6bc311feb3518ada11f7d9dbb7250543a36
SabrinaLaforest <70386265+SabrinaLaforest@users.noreply.github.com>
2022-12-15 21:15:30 -0500

    boutique searching

--- wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
+++ wordpress/wordpress/wp-content/themes/theme-de-base/boutique.php
@@ -5,42 +5,29 @@
 get_header();
 
 $products = array('post_type' => array('jouet', 'bijoux', 'accessoir', 'comestible'));
+$jouets = array('post_type' => array('jouet'));
+$bijoux = array('post_type' => array('bijoux'));
+$accessoires = array('post_type' => array('accessoir'));
+$comestibles = array('post_type' => array('comestible'));
 
 ?>
 
 <main>
-    <div class='searchingBoutique'>
+<div class='searchingBoutique'>
     <label>
-      Age:
-      <select name='Age:'>
-        <option value="4">0 à 4 ans</option>
-        <option value="8">5 à 8 ans</option>
-        <option value="9">9 à 18 ans</option>
-        <option value="adulte">Adultes</option>
-      </select>
-    </label>
-    
-    <label>
-      Tier par:
-      <select name='Trier par:'>
+      Catégories:
+      <select name='category' id='boutiqueCategory'>
         <option value="default">Par défault</option>
-        <option value="nouveautes">Nouveautés</option>
-        <option value="croissant">Prix croissant</option>
-        <option value="decroissant">Prix décroissant</option>
+        <option value="toy">Jouets</option>
+        <option value="jewlery">Bijoux</option>
+        <option value="other">Accessoires</option>
+        <option value="food">Comestible</option>
       </select>
     </label>
   </div>
     
     <article class='lesProduits'>
-        <?php $query = new WP_Query($products);
-        while($query->have_posts()) : $query->the_post(); ?>
-        <div class='produit'>
-            <img class='imgProduit' src='<?php the_field('image_produit') ; ?>'>
-            <div class='nomProduit produitGeneralInfo'><?php the_field('titre') ; ?></div>
-            <div class='prixProduit produitGeneralInfo'><?php the_field('prix') ; ?></div>
-            <button type="button" class='boutiqueBouton'>Ajouter au Panier</button>
-        </div>
-        <?php  endwhile; wp_reset_postdata(); ?>
+        
     </article>
 </main>
 
